e13beb7a46e55918504546b9b9079f867042340d
Rajat Kumar <21f1001805@ds.study.iitm.ac.in>
2026-07-23 16:50:49 +0530

    leaderbord fix

--- frontend/src/features/landing/components/ContainerScroll.tsx
+++ frontend/src/features/landing/components/ContainerScroll.tsx
@@ -11,6 +11,7 @@ export const ContainerScroll = ({
   const containerRef = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: containerRef,
+    offset: ['start end', 'center center'],
   });
   const [isMobile, setIsMobile] = React.useState(false);
 
@@ -35,11 +36,11 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="relative flex h-[60rem] items-center justify-center p-2 md:h-[80rem] md:p-20"
+      className="relative flex items-center justify-center px-2 py-16 md:px-20 md:py-24"
       ref={containerRef}
     >
       <div
-        className="w-full relative py-10 md:py-40"
+        className="w-full relative"
         style={{
           perspective: '1000px',
         }}
@@ -84,9 +85,9 @@ export const Card = ({
         boxShadow:
           '0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003',
       }}
-      className="mx-auto -mt-12 h-[30rem] w-full max-w-5xl rounded-[30px] border-4 border-[#6C6C6C] bg-[#222222] p-2 shadow-2xl md:h-[40rem] md:p-6"
+      className="mx-auto -mt-12 w-full max-w-7xl rounded-[30px] border-4 border-[#6C6C6C] bg-[#222222] p-2 shadow-2xl md:p-6"
     >
-      <div className="h-full w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4">
+      <div className="w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4">
         {children}
       </div>
     </motion.div>
