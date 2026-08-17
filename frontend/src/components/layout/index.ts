14316b3dac000e127438a644b3de6ebbf7065328
Rajat Kumar <21f1001805@ds.study.iitm.ac.in>
2026-08-17 14:14:03 +0530

    sidebar changes

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -2,6 +2,7 @@ export * from './Footer';
 export * from './header';
 export * from './LanguageSwitcher';
 export * from './Sidebar';
+export * from './SidebarPromo';
 export * from './ThemeToggle';
 export * from './TopBar';
 export * from './UserMenu';
